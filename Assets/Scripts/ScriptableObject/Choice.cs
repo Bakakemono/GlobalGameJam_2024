8030e2727fbe1c77db75efbe8f352bb84957f024
Bakakemono <robinalves1224@gmail.com>
2024-03-10 12:58:28 +0100

    Add text scriptable object

--- Assets/Scripts/ScriptableObject/Choice.cs
+++ Assets/Scripts/ScriptableObject/Choice.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Choice : ScriptableObject
 {
+    public string _doorMessage;
     public virtual void Effect() {}
 }
